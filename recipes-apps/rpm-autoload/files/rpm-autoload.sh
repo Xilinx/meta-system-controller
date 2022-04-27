e7d9c8fda17c5f51df0b1fdc91a54b5fc56667a7
Tejas Bhumkar <tejas.arvind.bhumkar@xilinx.com>
2022-04-27 14:43:21 +0530

    rpm-autoload.sh:Updated script to apply PL and PS Overlay.
    Updated script to apply the PL and PS device tree overlay using fpgautil command.
    
    Signed-off-by: Swagath Gadde <swagath.gadde@xilinx.com>
    Signed-off-by: Tejas Bhumkar <tejas.arvind.bhumkar@xilinx.com>
    Signed-off-by: Mark Hatle <mhatle@xilinx.com>

--- recipes-apps/rpm-autoload/files/rpm-autoload.sh
+++ recipes-apps/rpm-autoload/files/rpm-autoload.sh
@@ -4,11 +4,13 @@ dev_eeprom=$(find /sys/bus/i2c/devices/*54/ -name eeprom | head -1)
 manufacturer=$(fru-print.py -b som -s $dev_eeprom -f manufacturer)
 board=$(fru-print.py -b som -s $dev_eeprom -f product | tr '[:upper:]' '[:lower:]')
 revision=$(fru-print.py -b som -s $dev_eeprom -f revision | tr '[:upper:]' '[:lower:]')
+revision_ps=$(echo $revision | cut -b 1 | tr '[:lower:]' '[:upper:]')
 
 echo "BOARD:$board REVISION:$revision"
 
 base_path="/lib/firmware/xilinx/${board}-${revision}*/"
 dtbo_file="${board}-${revision}*.dtbo"
+psdtbo_file="zynqmp-sc-$board-rev$revision_ps.dtbo"
 bit_file="${board}-${revision}*.bit.bin"
 
 if [ -d "/run/media/mmcblk0p2/" ]; then
@@ -18,7 +20,7 @@ elif [ -d "/mnt/sd-mmcblk0p2/" ]; then
 fi
 
 
-if [ -d ${base_path} ]  && [ -f ${base_path}/$dtbo_file ] && [ -f ${base_path}/$bit_file ]; then
+if [ -d ${base_path} ]  && [ -f ${base_path}/$dtbo_file ] && [ -f ${base_path}/$bit_file ] && [ -f ${base_path}/$psdtbo_file ]; then
         echo "RPM Already installed at ${base_path}"
 else
         if [ -f ${partition_path}/${board}-${revision}*.rpm ]; then
@@ -29,9 +31,11 @@ else
         fi
 fi
 
-if [ -f ${base_path}/$dtbo_file ] && [ -f ${base_path}/$bit_file ]; then
+if [ -f ${base_path}/$dtbo_file ] && [ -f ${base_path}/$bit_file ] && [ -f ${base_path}/$psdtbo_file ]; then
         echo "Applying $bit_file and $dtbo_file"
-        fpgautil -b ${base_path}/$bit_file -o ${base_path}/$dtbo_file
+	fpgautil -b ${base_path}/$bit_file -o ${base_path}/$dtbo_file -f Full -n Full
+	echo "Applying $psdtbo_file"
+	fpgautil -o ${base_path}/$psdtbo_file -n psdtbo
 else
         echo "RPM did not install properly, please check ${base_path}"
 fi
