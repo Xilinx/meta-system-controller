94c49310baac8139fb2018c92c1f035e0875d5e5
Jesus De Haro <jesus.de-haro@amd.com>
2024-02-29 10:53:31 -0800

    system-controller-app: Add script for generating system-controller-app.inc
    Changelogs:
      sc-app-repo-generate-srcuri.sh added for generating system-controller-app.inc
    Signed-off-by: Mark Hatle <mark.hatle@amd.com>
    (cherry picked from commit 258de6dd8c06774bd3f4825242a8b81d8504868c)
    Signed-off-by: Mark Hatle <mark.hatle@amd.com>

---
+++ scripts/sc-app-repo-generate-srcuri.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+# Generate the system-controller-app.inc file
+#
+# Usage:
+# cd sources/meta-system-controller
+# ./scripts/sc-app-repo-generate-srcuri.sh \
+#    https://xcoartifactory.xilinx.com/artifactory/system-controller/sc_app_bsp/2023.2/2023.2_02232200/internal/ \
+#    > recipes-utils/system-conroller-app/system-controller-app.inc
+#
+# It is assumed the URL being pointed to will be a series of directories. The BOARD value
+# will be the directory name, and the contents within the directory will be the board
+# specific files.
+#
+
+if [ $# -lt 1 -o $# -gt 2 ]; then
+    echo "Usage: $0 <url> [<local_dir>]" >&2
+    exit 1
+fi
+
+url=$(echo $1 | sed -e 's,/$,,')
+
+localdir=${2}
+
+urlproto=$(echo $url | sed -e 's,://.*,://,')
+urlpath=$(echo $url | sed -e 's,'${urlproto}',,')
+
+if [ ${url} = ${urlproto} -o -z ${url} ]; then
+    echo "URL $url is invalid" >&2
+    exit 1
+fi
+
+if [ ${urlproto} = "file://" ]; then
+    # file:// URL, usually only for testing
+    cd ${urlpath}
+elif [ -n "${localdir}" ]; then
+    # Remote url, but using a local cache to generate
+    cd ${localdir}
+else
+    # Remove url, recursive fetch and then generate
+    tempdir=$(mktemp -d)
+    cd ${tempdir}
+    wget --recursive --no-parent $url/
+
+    cd ${urlpath}
+fi
+
+echo "# Automatically generated.  Manual changes will be lost."
+echo
+echo "ES1_PATH = \"\${@d.getVarFlag('ES1_PATH', d.getVar('BOARD')) or ''}\""
+echo "SYS_PATH = \"\${@d.getVarFlag('SYS_PATH', d.getVar('BOARD')) or ''}\""
+echo "ELF_PATH = \"\${@d.getVarFlag('ELF_PATH', d.getVar('BOARD')) or ''}\""
+echo "JSON_PATH = \"\${@d.getVarFlag('JSON_PATH', d.getVar('BOARD')) or ''}\""
+echo "JPG_PATH = \"\${@d.getVarFlag('JPG_PATH', d.getVar('BOARD')) or ''}\""
+echo "PNG_PATH = \"\${@d.getVarFlag('PNG_PATH', d.getVar('BOARD')) or ''}\""
+echo "JS_PATH = \"\${@d.getVarFlag('JS_PATH', d.getVar('BOARD')) or ''}\""
+echo
+echo "SC_APP_REPO = \"git://github.com/Xilinx/system-controller-app.git\""
+echo "SC_APP_BRANCH = \"xlnx_rel_v2023.2\""
+echo "SC_APP_SRCREV = \"c61a9a8d8a0e86a4f292ce2b70730039650e3c84\""
+echo
+
+dirnames=$(curl --silent ${urlproto}${urlpath}/ | grep -o 'href=".*">' | sed 's/href="//;s/\/">//')
+for dir in $dirnames; do 
+	if [ "$dir" == ".." ]; then
+		continue
+	fi
+	files=$(curl --silent "${urlproto}${urlpath}/$dir/" | grep -o 'href=".*">' | sed -e "s/href=\"//g" | sed -e 's/">//g')
+	echo "# $dir"
+	device=$(basename "$dir")
+	device="${device,,}"
+	for i in $files; do
+		if [ "$i" == "../" ]; then
+			continue
+		fi
+		echo "# $i"
+		if [[ "$i" == *"elf"* ]]; then
+			name=$device-elf
+			yp_name="ELF_PATH"
+		elif [[ "$i" == *"pdi"* ]] && [[ "$i" == *"es1"* ]]; then
+			name=$device-es1-system
+			yp_name="ES1_PATH"
+		elif [[ "$i" == *"pdi"* ]]; then
+			name=$device-system
+			yp_name="SYS_PATH"
+		elif [[ "$i" == *"json"* ]]; then
+			name=$device-json
+			yp_name="JSON_PATH"
+		elif [[ "$i" == *"strings"* ]]; then
+			name=$device-js
+			yp_name="JS_PATH"
+		elif [[ "$i" == *"jpg"* ]]; then
+			name=$device-jpg
+			yp_name="JPG_PATH"
+		elif [[ "$i" == *"png"* ]]; then
+			name=$device-png
+			yp_name="PNG_PATH"
+		fi
+		echo ${yp_name}["${device}"] = "'${urlproto}${urlpath}/${dir}/${i};name=${name}'"
+		curl -o tempfile --silent "${urlproto}${urlpath}/$dir/$i"
+		echo SRC_URI["${name}".sha256sum] = "'$(sha256sum tempfile | awk '{print $1}')'"
+		echo
+	done
+done
+rm tempfile
