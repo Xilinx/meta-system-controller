Xilinx/meta-system-controller

commit 1663a9cf269406b7e3d85e5c93286a280eabdefa

repartition and format/resize
Replace the previous, one-off, home-brew resizing mechanism with one based on
systemd-repart.

The eMMC on the VEK280 board, for example, is currently 32GB. The Xilinx Boot
Image Recovery Tool (BIRT) does not allow the user to flash an image that is
larger than 4GB. Even at that, flashing a 2.5GB image takes about 10 minutes,
any sort of image used for an A/B+data setup would be quite large and take a
very long time to flash even if it were possible. With this mechanism were
are able to create and ship small images which then repartition and expand
at boot to setup additional partitions and filesystems based on the provided
configuration.

Unfortunately the systemd-repart mechanism in langdale (i.e. 2023), i.e.
systemd v251, appears to have a bug whereby the unit file runs too early in
the boot, such that the drive's /dev nodes are not setup and the tool has
nothing to work on. Attempts to move the provided unit file elsewhere in the
boot order is met with systemd finding dependency loops in the service file
ordering. Therefore a new service file was created, one that won't conflict
with existing service files.

This has all been tested on a more recent version of systemd (v255) and works
perfectly out-of-the-box with just the configuration files. Therefore at that
point remove the temp-repart service file and place the config files directly
in /etc/repart.d (instead of /etc/t-repart.d). Probably easiest to simply
use a systemd bbappend at that point. The config files for this new service
file were placed in an alternate directory so that we could still install and
use the systemd-repart tool but without triggering it to try running (and
reporting failures during system bring-up).

This change is eval-brd-sc-zynqmp specific.
Tested on the system-controller of a VEK280 board.

Signed-off-by: Trevor Woerner <trevor.woerner@amd.com>
Signed-off-by: Mark Hatle <mark.hatle@amd.com>

recipes-core/temp-repart/temp-repart_1.0.bb
@@ -1,0 +1,43 @@
+# Copyright (C) 2024, Advanced Micro Devices, Inc. All rights reserved.
+# SPDX-License-Identifier: MIT
+
+SUMMARY = "Repartition, format, resize, edit /etc/fstab, and mount"
+DESCRIPTION = "Work-around for bad systemd-repart dependencies in systemd-v251. \
+This recipe is not needed anymore with systemd-v255 (scarthgap), simply remove \
+and replace with regular systemd-repart. Put the config files in \
+${D}${sysconfdir}/repart.d and get rid of the *service and *sh files."
+LICENSE = "MIT"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
+
+FILESEXTRAPATHS:prepend:eval-brd-sc-zynqmp := "${THISDIR}/files:"
+
+PACKAGE_ARCH = "${MACHINE_ARCH}"
+
+inherit systemd
+
+SRC_URI:eval-brd-sc-zynqmp = " \
+	file://25-rootfsA.conf \
+	file://35-rootfsB.conf \
+	file://45-data.conf \
+	file://temp-repart.service \
+	file://temp-repart.sh \
+	"
+
+do_install:eval-brd-sc-zynqmp() {
+	install -d ${D}${sysconfdir}/temp-repart.d/
+	install -m 0644 ${WORKDIR}/25-rootfsA.conf ${D}${sysconfdir}/temp-repart.d/
+	install -m 0644 ${WORKDIR}/35-rootfsB.conf ${D}${sysconfdir}/temp-repart.d/
+	install -m 0644 ${WORKDIR}/45-data.conf ${D}${sysconfdir}/temp-repart.d/
+
+	install -d ${D}${sysconfdir}/systemd/system
+	install -m 0644 ${WORKDIR}/temp-repart.service ${D}${sysconfdir}/systemd/system/
+
+	install -d ${D}${base_bindir}
+	install -m 0755 ${WORKDIR}/temp-repart.sh ${D}${base_bindir}/
+}
+
+SYSTEMD_AUTO_ENABLE = "enable"
+SYSTEMD_SERVICE:${PN} = "temp-repart.service"
+
+FILES:eval-brd-sc-zynqmp:${PN} += "${sysconfdir}/temp-repart.d/ ${sysconfdir}/systemd/system/*.service"
+RDEPENDS:eval-brd-sc-zynqmp:${PN} = "e2fsprogs-mke2fs"

recipes-core/base-files/base-files_%.bbappend
@@ -1,0 +1,1 @@
+dirs755:append:eval-brd-sc-zynqmp = " /data"

recipes-core/images/system-controller-image-full-cmdline.bb
@@ -20,7 +20,6 @@ IMAGE_INSTALL = " \
     packagegroup-petalinux-syscontroller \
     python3-pip \
     python3-psutil \
-    resize-partition \
     u-boot-tools \
     udev-extraconf \
     uboot-device-tree \
@@ -30,3 +29,5 @@ IMAGE_INSTALL = " \
 "
 
 IMAGE_INSTALL:append:system-controller = " raft"
+IMAGE_INSTALL:append:eval-brd-sc-zynqmp = " temp-repart"
+IMAGE_INSTALL:append:vck-sc-zynqmp = " resize-partition"

recipes-core/temp-repart/files/eval-brd-sc-zynqmp/25-rootfsA.conf
@@ -1,0 +1,6 @@
+# Copyright (C) 2024, Advanced Micro Devices, Inc. All rights reserved.
+# SPDX-License-Identifier: MIT
+[Partition]
+Type=root
+Weight=250
+SizeMaxBytes=5G

recipes-core/temp-repart/files/eval-brd-sc-zynqmp/35-rootfsB.conf
@@ -1,0 +1,9 @@
+# Copyright (C) 2024, Advanced Micro Devices, Inc. All rights reserved.
+# SPDX-License-Identifier: MIT
+[Partition]
+Type=root
+Label=rootfsB
+Weight=250
+SizeMaxBytes=5G
+Format=ext4
+GrowFileSystem=yes

recipes-core/temp-repart/files/eval-brd-sc-zynqmp/45-data.conf
@@ -1,0 +1,8 @@
+# Copyright (C) 2024, Advanced Micro Devices, Inc. All rights reserved.
+# SPDX-License-Identifier: MIT
+[Partition]
+Type=root-secondary
+Label=data
+Weight=500
+Format=ext4
+GrowFileSystem=yes

recipes-core/temp-repart/files/eval-brd-sc-zynqmp/temp-repart.service
@@ -1,0 +1,17 @@
+# Copyright (C) 2024, Advanced Micro Devices, Inc. All rights reserved.
+# SPDX-License-Identifier: MIT
+
+[Unit]
+Description=Repartition Root Disk
+DefaultDependencies=no
+After=multi-user.target
+ConditionVirtualization=!container
+ConditionDirectoryNotEmpty=|/etc/temp-repart.d
+
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+ExecStart=/bin/temp-repart.sh
+
+[Install]
+WantedBy=multi-user.target

recipes-core/temp-repart/files/eval-brd-sc-zynqmp/temp-repart.sh
@@ -1,0 +1,12 @@
+#!/bin/sh
+# Copyright (C) 2024, Advanced Micro Devices, Inc. All rights reserved.
+# SPDX-License-Identifier: MIT
+
+/bin/systemd-repart --dry-run=no --definitions=/etc/temp-repart.d
+/lib/systemd/systemd-growfs /
+
+cat /etc/fstab | grep "LABEL=data" > /dev/null
+if [ $? -eq 1 ]; then
+	echo "LABEL=data /data auto defaults 0 0" >> /etc/fstab
+fi
+mount -a

commit 9a7ad18d979fad6399393e8d2471c3f676e43eee

temp-repart: OVERRIDE service file
The service file for this recipe was not under the same override as the rest
of this recipe's contents.

Fixes: 1663a9cf2694 ("repartition and format/resize")
Signed-off-by: Trevor Woerner <trevor.woerner@amd.com>
Signed-off-by: Sandeep Gundlupet Raju <sandeep.gundlupet-raju@amd.com>

recipes-core/temp-repart/temp-repart_1.0.bb
@@ -37,7 +37,7 @@ do_install:eval-brd-sc-zynqmp() {
 }
 
 SYSTEMD_AUTO_ENABLE = "enable"
-SYSTEMD_SERVICE:${PN} = "temp-repart.service"
+SYSTEMD_SERVICE:${PN}:eval-brd-sc-zynqmp = "temp-repart.service"
 
 FILES:eval-brd-sc-zynqmp:${PN} += "${sysconfdir}/temp-repart.d/ ${sysconfdir}/systemd/system/*.service"
 RDEPENDS:eval-brd-sc-zynqmp:${PN} = "e2fsprogs-mke2fs"