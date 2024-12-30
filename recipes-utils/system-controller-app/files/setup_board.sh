d162e95323767effa83db50d38cdd7f2bff1a852
Saeed Nowshadi <saeed.nowshadi@amd.com>
2024-12-30 13:10:15 -0800

    SC Update7 changes
    Search for EEPROM path referencing 'eeprom_cc' label.  Print sc_app commit id
    when getting version info.  Use correct path to reference 'version_info.sh'
    script when collecting logs.
    
    Signed-off-by: Saeed Nowshadi <saeed.nowshadi@amd.com>
    Signed-off-by: Mark Hatle <mark.hatle@amd.com>

--- recipes-utils/system-controller-app/files/setup_board.sh
+++ recipes-utils/system-controller-app/files/setup_board.sh
@@ -9,7 +9,7 @@ if [ -f /etc/profile.d/socks_proxy.sh ]; then
     . /etc/profile.d/socks_proxy.sh
 fi
 
-EEPROM=$(/bin/ls /sys/bus/i2c/devices/1-0054/eeprom 2> /dev/null || /bin/ls /sys/bus/i2c/devices/11-0054/eeprom 2> /dev/null)
+EEPROM=$(ls /sys/bus/i2c/devices/*/eeprom_cc*/nvmem 2> /dev/null)
 BOARD=$(/usr/sbin/ipmi-fru --fru-file="$EEPROM" --interpret-oem-data | /usr/bin/awk -F": " '/FRU Board Product/ { print tolower ($2) }')
 echo Install board packages for "$BOARD"
 if dnf install -y "packagegroup-systemcontroller-${BOARD}"; then
