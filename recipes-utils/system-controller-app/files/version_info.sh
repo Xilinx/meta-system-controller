a3e2ec30ac5fa07e55a542863835e728f4b72975
Naroju, Nithish Kumar <NithishKumar.Naroju@amd.com>
2026-05-22 11:56:12 +0530

    python3-image-mgmt:version_info.sh: Update SRCREV 0522 and update script with new utility (#69)
    * python3-image-mgmt: Update SRCREV 0522
    
    Changelog:
       src: Add multi-platform support for version extraction
    
    Signed-off-by: Nithish Kumar Naroju <nithishkumar.naroju@amd.com>
    
    * version_info.sh: Replace image_update with image-mgmt for QSPI version info
    
    Switch from the legacy /usr/bin/image_update tool to the new image-mgmt
    utility for retrieving QSPI image version information. The image-mgmt
    tool provides the same version output through its "version" subcommand
    and is the preferred interface going forward.
    
    Signed-off-by: Nithish Kumar Naroju <nithishkumar.naroju@amd.com>
    
    ---------
    
    Signed-off-by: Nithish Kumar Naroju <nithishkumar.naroju@amd.com>

--- recipes-utils/system-controller-app/files/version_info.sh
+++ recipes-utils/system-controller-app/files/version_info.sh
@@ -28,7 +28,7 @@ LEGACY_BOARD=$(if [ "${BOARD}" = "VCK190" ] || [ "${BOARD}" = "VMK180" ]; then e
 if [ "${LEGACY_BOARD}" -eq 0 ]; then
     MSG="QSPI Image Information"
     print_msg "${MSG}"
-    QSPI_INFO=$(/usr/bin/image_update -p 2>&1)
+    QSPI_INFO=$(image-mgmt version 2>&1)
 
     if echo "$QSPI_INFO" | grep -q "Unable to retrieve"; then
 	    echo "Unable to fetch boot.bin information from spi flash (layout mismatch)"
